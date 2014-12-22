de9c7e0bcfb2fd568a0732f25db48c5e5e3ac1e8
Dan Bucholtz <dan.bucholtz@gmail.com>
2014-12-22 13:32:05 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+soa-example-user-service-api
+============================
+
+Use this utility to access the authentication service in the sample SOA
---
+++ index.js
@@ -0,0 +1,44 @@
+var request = require('request');
+var Q = require("q");
+
+var utils = require("soa-example-core-utils");
+
+var config = require("soa-example-service-config").config();
+
+var authenticateUserByAccessToken = function(token){
+	var deferred = Q.defer();
+	
+	var url = utils.createBaseUrl(config.authenticationServiceIp, config.authenticationServicePort);
+
+	var toPost = {
+		accessToken : token
+	};
+
+	utils.postJson(toPost, url).then(function(response){
+		deferred.resolve(response);
+	});
+
+	return deferred.promise;
+};
+
+var authenticateUserByEmailAddressAndPassword = function(emailAddress, password){
+	var deferred = Q.defer();
+	
+	var url = utils.createBaseUrl(config.authenticationServiceIp, config.authenticationServicePort);
+
+	var toPost = {
+		emailAddress : emailAddress,
+		password: password
+	};
+
+	utils.postJson(toPost, url).then(function(response){
+		deferred.resolve(response);
+	});
+
+	return deferred.promise;
+};
+
+module.exports = {
+	authenticateUserByAccessToken : authenticateUserByAccessToken,
+	authenticateUserByEmailAddressAndPassword : authenticateUserByEmailAddressAndPassword
+}
